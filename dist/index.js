77bf1f4f6cdd0bc25878beb83a7488585f815004
Mike Hoffert <mike.hoffert@gmail.com>
2018-02-06 16:56:58 -0700

    Dist files

--- dist/index.js
+++ dist/index.js
@@ -9,19 +9,52 @@ var _lodash = require("lodash");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = {
-  page: function page(_ref) {
-    var filters = _ref.filters,
-        _page = _ref.page,
-        sortCol = _ref.sortCol,
-        sortDir = _ref.sortDir;
+  inList: function inList(data, value) {
+    var flag = false;
+    for (var i = 0; i < data.length; i++) {
+      if (data[i]["value"] === value) {
+        flag = true;
+        break;
+      }
+    }
+    return flag;
+  },
+  filterPage: function filterPage(_ref) {
+    var _this = this;
+
+    var column = _ref.column,
+        filter = _ref.filter;
+
+    var data = [];
+    (0, _lodash.cloneDeep)(_people2.default).forEach(function (person) {
+      if ((0, _lodash.isNil)(filter) || (0, _lodash.isEmpty)(filter) || person[column].indexOf(filter) > -1) {
+        if (!_this.inList(data, person[column])) {
+          data.push({
+            id: person.id,
+            value: person[column]
+          });
+        }
+      }
+    });
+
+    return {
+      total: data.length,
+      data: (0, _lodash.orderBy)(data, ["value"], ["asc"]).slice(0, 10)
+    };
+  },
+  page: function page(_ref2) {
+    var filters = _ref2.filters,
+        _page = _ref2.page,
+        sortCol = _ref2.sortCol,
+        sortDir = _ref2.sortDir;
 
     var start = (_page - 1) * 10;
     var end = _page * 10;
     var data = (0, _lodash.filter)(_people2.default, function (person) {
       var flag = true;
-      filters.forEach(function (_ref2) {
-        var name = _ref2.name,
-            values = _ref2.values;
+      filters.forEach(function (_ref3) {
+        var name = _ref3.name,
+            values = _ref3.values;
 
         var matches = false;
         values.forEach(function (value) {
@@ -36,6 +69,7 @@ module.exports = {
 
       return flag;
     });
+
     return {
       total: data.length,
       data: (0, _lodash.orderBy)(data, [sortCol], [sortDir]).slice(start, end)
